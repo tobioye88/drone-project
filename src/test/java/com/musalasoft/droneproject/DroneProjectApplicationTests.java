b6e1e6ea28383aebdb823da16d4853236298d7dc
tobioye88 <tobioye88@gmail.com>
2021-11-20 17:37:01 +0100

    Initial Commit

---
+++ src/test/java/com/musalasoft/droneproject/DroneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musalasoft.droneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
